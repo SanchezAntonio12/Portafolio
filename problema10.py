b97a2b62db5663a75d31405bbdc830c97ab22331
SanchezAntonio12 <sanchezre27@gmail.com>
2024-06-18 16:48:03 -0500

    problema10

---
+++ problema10.py
@@ -0,0 +1,11 @@
+print("----------------------------")
+print("Comparacion de dos numero")
+print("----------------------------")
+N1 = float(input("Ingresa el primer N1: "))
+N2 = float(input("Ingresa el segundo N2: "))
+if N1 > N2:
+    print("N1es mayor que N2.")
+elif N1 < N2:
+    print("N2 es mayor que N1.")
+else:
+    print("Los números son iguales.")
---
+++ problema15.py
@@ -0,0 +1,12 @@
+print("----------------------------")
+print("Años de experencia")
+print("----------------------------")
+año = float(input("Ingresa los años de experiencia: "))
+if año < 2:
+    print("Junior")
+elif 2 <= año <5:
+    print("Semi-Serio")
+else:
+    print("Serio")
+
+print("La categoría del trabajador es: ")
