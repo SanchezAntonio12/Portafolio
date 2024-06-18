4876d85f9961415846298e576c74f0a849ea134e
SanchezAntonio12 <sanchezre27@gmail.com>
2024-06-18 16:42:41 -0500

    problema9

---
+++ problema8.py
@@ -0,0 +1,8 @@
+print("----------------------------")
+print("Rango de numero")
+print("----------------------------")
+numero = float(input("Ingresa un número: "))
+if  (1 <= numero <= 100):
+    print("El numero está entre 1 y 100.")
+else:
+    print("El numero no está entre 1 y 100.")
