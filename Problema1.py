728239393ab7398f6e7b1c7aa5d54aa17135adac
SanchezAntonio12 <sanchezre27@gmail.com>
2024-06-18 15:25:55 -0500

    Problema1

---
+++ Problema1.py
@@ -0,0 +1,10 @@
+print("----------------------------")
+print("Verifica si es positivo, negativo o cero")
+print("----------------------------")
+numero = float(input("ingrese el numero: "))
+if numero > 0:
+   print("El número es positivo. ")
+elif numero < 0:
+   print("El número es negativo. ")
+else:
+   print("El número es cero. ") 
