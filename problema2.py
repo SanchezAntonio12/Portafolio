3753fe27af7d0499924b69150955cf42177f2fa7
SanchezAntonio12 <sanchezre27@gmail.com>
2024-06-18 16:25:33 -0500

    problema2

---
+++ problema2.py
@@ -0,0 +1,8 @@
+print("----------------------------")
+print("Edad de una persona adolecente")
+print("----------------------------")
+edad = float(input("ingrese su edad: "))
+if edad >= 12 and edad <= 17:
+        print("adolescente.")
+elif edad >= 18 and edad <= 19:
+        print("adulto.")
